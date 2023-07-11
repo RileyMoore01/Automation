9062e24676fad401c27273568ffc3f2f5668231c
RileyMoore01 <xrileyxmoorex@outlook.com>
2023-07-11 16:56:40 -0500

    added looping feature

--- AutoCommit.py
+++ AutoCommit.py
@@ -7,8 +7,7 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
-
-# Set up firefox browse
+# set up browser
 firefoxOptions = Options()
 firefoxOptions.binary_location = r"C:\\Program Files\\Mozilla Firefox\\firefox.exe"
 service = Service()
@@ -37,23 +36,35 @@ def loginToGitHub():
     submitLogin = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/main/div/div[4]/form/div/input[13]")
     submitLogin.click()
 
-    driver.implicitly_wait(100)
+    driver.implicitly_wait(500)
+
+
 
 # <summary>
 # Open the bootcamp repo to edit
 # </summary>
 def openRepository():
     bootcamp = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/div/aside/div/div/loading-context/div/div[1]/div/ul/li[3]/div/div/a")
+    driver.implicitly_wait(100)
     bootcamp.click()
+
+    driver.implicitly_wait(100)
+
+
+# <summary>
+# Open the file to be edited
+# </summary>
+def openFile():
+    # click on file to edit
+    file = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/div/div/div/div[2]/div[1]/div[4]/div[3]/div[1]/div[6]/div[2]/span/a")
+    file.click()
+
+    driver.implicitly_wait(100)
 
 # <summary>
 # Open the edit page to create a difference to commit
 # </summary>
 def editRepository():
-
-    # click on file to edit
-    file = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/div/div/div/div[2]/div[1]/div[4]/div[3]/div[1]/div[6]/div[2]/span/a")
-    file.click()
 
     # start editing
     editBtn = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/react-app/div/div/div[2]/div[1]/div/div/main/div[2]/div/div[3]/div[3]/div/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/span/a")
@@ -70,6 +81,8 @@ def commit():
     commit = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/react-app/div/div/div[2]/div[1]/div/div/main/div[2]/div/div[3]/div[1]/div[2]/button")
     commit.click()
 
+    driver.implicitly_wait(100)
+
     commitConfirm = driver.find_element(By.XPATH, "/html/body/div[1]/div[6]/div/main/turbo-frame/react-app/div/div/div[1]/div/div/div/div[3]/button[2]")
     commitConfirm.click()
 
@@ -83,12 +96,21 @@ def killDriver():
 
 # ----------- Function Calls -------------
 
+count = 0   # always set to 0
+limit = 5   # set to how many times you would like to run
+
 loginToGitHub()
 
 openRepository()
 
-editRepository()
+openFile()
 
-commit()
+while (count <= limit):
+
+    editRepository()
+
+    commit()
+
+    count = count + 1
 
 killDriver()
